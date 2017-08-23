104b510da8db6af2aacfef38cb57caaa3ed17442
users <users@git.com>
2017-08-23 18:22:07 +0800

    change1

---
+++ src/pages/order/index.vue
@@ -0,0 +1,41 @@
+<template>
+
+<div>
+  <div>{{name}}</div>
+  <div>
+    xxxxxxxxx
+  </div>
+  <div class="img">
+    <img @click="handleclick" src="http://img0.imgtn.bdimg.com/it/u=3160968260,2383732736&fm=26&gp=0.jpg" alt="">
+  </div>
+</div>
+  
+	
+</template>
+<script src="../dist/js/swiper.min.js"></script>
+
+<script>
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      name: 'order page',
+      handleclick:function(){
+        alert("xxxxxxxxaaa")
+      }
+    }
+  }
+  
+}
+</script>
+
+
+<style scoped>
+ 
+	.img img{
+    width: 100%;
+    height: 600px;
+    
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
+import Order from '@/pages/order/index'
 import City from '@/pages/city/index'
 
 Vue.use(Router)
@@ -16,6 +17,10 @@ export default new Router({
       path: '/city',
       name: 'city',
       component: City
+    },{
+      path: '/order',
+      name: 'order',
+      component: Order
     }
   ]
 })

c662b10e0931527dfd4fa7bae13d01d3fa2c9248
zhj <1589990956@qq.com>
2017-08-23 16:24:24 +0800

    one

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,6 +3,9 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
+
+
+    
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
@@ -25,7 +28,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App' 
     }
   }
 }

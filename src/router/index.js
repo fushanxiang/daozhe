82bb9a70731a1053e61c2ef0c27ce8383bfa2358
users <users@git.com>
2017-08-23 18:26:41 +0800

    change2

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Index from '@/pages/index/index'
+
 import Order from '@/pages/order/index'
-import City from '@/pages/city/index'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index
-    },
-    {
-      path: '/city',
-      name: 'city',
-      component: City
-    },{
+  {
       path: '/order',
       name: 'order',
       component: Order

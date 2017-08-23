ba5b35eb01f2c13b89a2be4c6cc9ce5f63831d9c
dandan <422850119@qq.com>
2017-08-23 17:13:34 +0800

    change

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>门票景点</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li><a href="https://vuejs.org" target="_blank">首页</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">列表</a></li>
+      <li><a href="https://gitter.im/vuejs/vue" target="_blank">推荐</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">销量</a></li>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">我的项目</a></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="http://router.vuejs.org/" target="_blank">你的</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">我的</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">他的</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">故宫</a></li>
     </ul>
   </div>
 </template>
@@ -25,7 +25,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Vue 一日游项目'
     }
   }
 }

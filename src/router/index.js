8433ba8d790dbb4313f8323c78bce049f172724e
admins <547726690@qq.com>
2017-08-23 17:14:07 +0800

    咱组的分支

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import City from '@/pages/city/index'
+import ParkDetail from '@/pages/parkdetail/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,10 @@ export default new Router({
       path: '/city',
       name: 'city',
       component: City
+    },{
+      path: '/parkdetail',
+      name: 'parkdetail',
+      component: ParkDetail
     }
   ]
 })

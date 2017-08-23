806997842799aba1849e563f80d343cb7d9fc1b1
liuchanghong <1256875721@qq.com>
2017-08-23 18:55:21 +0800

    liuchanghongChange

---
+++ src/pages/weekend/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{pageName}}</div>
+</template>
+
+<script>
+export default {
+  name: 'weekend',
+  data () {
+    return {
+    	pageName:'weekend page'
+    }
+     
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import City from '@/pages/city/index'
+import Weekend from '@/pages/Weekend/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/city',
       name: 'city',
       component: City
+    },
+    {
+      path: '/weekend',
+      name: 'weekend',
+      component: Weekend
     }
   ]
 })

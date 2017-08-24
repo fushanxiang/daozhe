e9dc862d2e4901e20cd5685ace15c1c4a973b43b
Mr_patience <823130507@qq.com>
2017-08-24 17:45:14 +0800

    change

---
+++ src/pages/tackitSale/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script>
+export default {
+  name: 'tackitSale',
+  data () {
+    return {
+    	name:"这是门票特卖页面"
+    }
+     
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import City from '@/pages/city/index'
+import TackitSale from '@/pages/TackitSale/index'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/city',
       name: 'city',
       component: City
+    },
+    {
+      path: '/tackitSale',
+      name: 'tackitSale',
+      component: TackitSale
     }
   ]
 })

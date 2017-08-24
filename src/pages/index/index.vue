7433066447a4f94061640723eb136e33ddcf4beb
shenglong.li <913937000@qq.com>
2017-08-24 16:56:06 +0800

    change

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>index</div>
+	<div>
+		<index-header></index-header>
+		<img-swiper></img-swiper>
+    <icon-swiper></icon-swiper>
+	</div>
 </template>
 
 <script>
+
+import IndexHeader from './header.vue'
+import ImgSwiper from './imgswiper.vue'
+import IconSwiper from './iconswiper.vue'
+
 export default {
   name: 'index',
   data () {
     return {
     }
      
+  },
+  components:{
+  	"index-header": IndexHeader,
+  	"img-swiper": ImgSwiper,
+    "icon-swiper": IconSwiper
   }
 }
 </script>

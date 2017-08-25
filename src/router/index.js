471dd6a5884d6853fcd9ce7d859f6f6c1ea7a1ba
jinzhou <1979008763@qq.com>
2017-08-25 22:16:13 +0800

    dsf

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import City from '@/pages/city/index'
-<<<<<<< HEAD
 import Weekend from '@/pages/Weekend/index'
-=======
 import BigCity from '@/pages/city/big_city'
 import SmallCity from '@/pages/city/small_city'
->>>>>>> origin/master
 
 Vue.use(Router)
 
@@ -21,26 +18,23 @@ export default new Router({
     {
       path: '/city/:id',
       name: 'city',
-<<<<<<< HEAD
       component: City
     },
     {
       path: '/weekend',
       name: 'weekend',
       component: Weekend
-=======
-      component: City,
-      children: [
-        {
-          path: 'bigCity',
-          component: BigCity
-        },
-        {
-          path: 'smallCity',
-          component: SmallCity
-        }
-      ]
->>>>>>> origin/master
-    }
-  ]
+    }],
+
+    children: [
+      {
+        path: 'bigCity',
+        component: BigCity
+      },
+      {
+        path: 'smallCity',
+        component: SmallCity
+      }
+    ]
+  
 })

cbc131388b1aa8375df533bfa1d0f022d8957fd8
Mr_patience <823130507@qq.com>
2017-08-25 11:33:15 +0800

    change

---
+++ src/pages/ticketSale/index.vue
@@ -0,0 +1,208 @@
+<template>
+	<div class="ticket-sale">
+		<div class="sale-header">
+             <a class="sale-header-back iconfont" href="javascript:window.history.back();">&#xe624;</a>
+            <h1 class="sale-header-title">门票特卖</h1>
+        </div>
+        <div class="ticket-navbar">
+            <span class="nav-title">促销门票</span>
+            <p class="nav-city">
+                <span class="city-name">北京</span>
+                <i class="iconfont">&#xe65e;</i>
+             </p>
+        </div>
+
+        <div class="product-list">
+            <ul class="sale-list">
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+
+                <li class="list-item">
+                    <img  class="sale-img" src="//img1.qunarzz.com/sight/p0/1704/6c/6c8f90546cca86c8a3.img.jpg_640x266_3aa718bd.jpg" alt="[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票"/>
+                    <h5 class="sale-title">[北京]【1元秒杀】北京蟒山公园国际奇幻森林灯光节门票</h5>
+                    <div class="sale-price">
+                        <span class="new-price">￥<i>1</i></span>
+                        <span class="old-price">￥<i>30</i></span>                    
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="sale-bottom">
+            <i class="iconfont">&#xe6ed;</i>
+            <strong>票面价</strong>
+            是指通过景区指定窗口售卖的纸质门票上标注的价格    
+        </div>
+	</div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+     
+    }
+     
+  }
+}
+</script>
+
+<style scoped>
+    @import "../../assets/font/iconfont.css";
+    .ticket-sale{
+      width:100%; 
+      position: relative;     
+    }
+    .sale-header{
+          position: relative;
+          height: .88rem;
+          border-bottom: #1b7a8b 1px solid;
+          z-index: 91;
+          background:#25a4bb;
+    }
+    .sale-header-back{
+          position: absolute;
+          left: 0;
+          top: 0;
+          width: .84rem;
+          height: .88rem;
+          line-height: .88rem;
+          text-align: center;
+          font-size: .36rem;
+          color: #fff;
+    }
+    .sale-header-title{      
+        overflow: hidden;
+        margin: 0 1rem;
+        line-height: .88rem;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        font-size: .32rem;
+        text-align: center;
+        color: #fff;
+    }
+    .ticket-navbar{    
+        padding: .2rem .2rem;
+        background-color: #fff;
+        position: relative;
+        background:yellow;
+    }
+    .nav-city{
+        float:right;
+    }
+    .product-list{
+        width:100%;
+        overflow:hidden;
+    }
+    .sale-list{
+        width:100%;
+    }
+    .list-item{
+        width:100%;    
+    }
+    .list-item .sale-img{
+        width:100%;
+         background-image: url(../../assets/img/sale-bg.jpg);
+
+    }
+    .sale-title{
+        width:80%;
+        height:.2rem;
+        margin:.2rem;
+        height:30px;
+        float:left;
+
+    }
+    .sale-price{
+        float:right;
+        height:30px;
+        padding:.2rem;
+    }
+    .sale-price .new-price,.sale-price .old-price{
+        width:100%;
+        line-height:.3rem;
+        display:block;
+    }
+    .new-price{
+        color:red;
+        font-size:.2rem;
+    }
+    .sale-bottom{
+        margin-top: .2rem;
+        padding: .2rem;
+        background: yellow;
+        color: #333;
+        font-size: .24rem;
+        line-height: .3rem;
+}
+</style>

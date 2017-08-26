20a9558cb33653cd606e42a9f055dd649fad1ae9
user <users@git.com>
2017-08-26 11:09:49 +0800

    change

---
+++ src/pages/onedaytour/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+		<index-header></index-header>
+		<img-swiper></img-swiper>
+    	<icon-swiper></icon-swiper>
+	</div>
+</template>
+
+<script>
+import IndexHeader from '../index/header.vue'
+import ImgSwiper from '../index/imgswiper.vue'
+import IconSwiper from '../index/iconswiper.vue'
+
+
+export default {
+  name: 'onedaytour',
+  data () {
+    return {
+    }
+     
+  },
+  components:{
+  	"index-header": IndexHeader,
+  	"img-swiper": ImgSwiper,
+    "icon-swiper": IconSwiper
+  }
+}
+</script>
+
+
+<style scoped>
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+<<<<<<< Updated upstream
 import Hello from '@/components/Hello'
+=======
+import Index from '@/pages/index/index'
+import City from '@/pages/city/index'
+import Onedaytour from '@/pages/onedaytour/index'
+import BigCity from '@/pages/city/big_city'
+import SmallCity from '@/pages/city/small_city'
+>>>>>>> Stashed changes
 
-Vue.use(Router)
+Vue.use(Router)  
 
 export default new Router({
   routes: [
     {
       path: '/',
+<<<<<<< Updated upstream
       name: 'Hello',
       component: Hello
+=======
+      name: 'index',
+      component: Index
+    },{
+      path: '/onedaytour',
+      name: 'onedaytour',
+      component: Onedaytour
+    },{
+      path: '/city/:id',
+      name: 'city',
+      component: City,
+      children: [
+        {
+          path: 'bigCity',
+          component: BigCity
+        },
+        {
+          path: 'smallCity',
+          component: SmallCity
+        }
+      ]
+>>>>>>> Stashed changes
     }
   ]
 })

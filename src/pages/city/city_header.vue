3b5f137a65b2b784cd8fe5d8fb40b8248448beff
lishihao <1144037097@qq.com>
2017-08-26 11:52:09 +0800

    aaa

---
+++ src/pages/city/city_header.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="city-header-area">
+        <span class="iconfont city-header-goback">&#xe624;</span>
+        <span class="city-china city-area" v-on:click="handleChina">国内</span>
+        <span class="city-abroad city-area" v-on:click="handleAbroad">海外</span>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data () {
+            return {}
+        },
+        methods: {
+            handleChina() {
+                location.href='http://localhost:8080/#/city';
+            },
+            handleAbroad() {
+                location.href='http://localhost:8080/#/city/abroadCity';
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    @import "../../assets/font/iconfont.css";
+  	.city-header-area {
+        line-height: .88rem;
+        background: #00afc7;
+        text-align: center;
+        overflow: hidden;
+    }
+    .city-header-goback {
+        font-size: .36rem;
+        color: #fff;
+        font-weight: bold;
+        float: left;
+        padding: 0 .22rem;
+    }
+    .city-area {
+        border: 1px solid white;
+        display: inline-block;
+        line-height: .56rem;
+        width: 2.04rem;
+        color: white;
+    }
+    .city-china {
+        background: white;
+        color: #00afc7;
+        border-radius: 3px 0 0 3px;
+        float: left;
+        margin-left: .44rem;
+        margin-top: .14rem;
+    }
+    .city-abroad {
+        border-radius: 0 3px 3px 0;
+        float: left;
+        margin-top: .14rem;
+    }
+</style>
--- src/pages/city/index.vue
+++ src/pages/city/index.vue
@@ -1,47 +1,23 @@
 <template>
-<<<<<<< HEAD
-    <hot-city></hot-city>
-=======
     <div>
-      	<div class="city-header">
-            <div class="city-header-area">
-                <span class="city-in-china city-area" v-on:click="handleChina">国内</span>
-                <span class="city-area" v-on:click="handleAbroad">海外</span>
-            </div>
-        </div>
+        <city-header></city-header>
+        <hot-city></hot-city>
         <router-view></router-view>
     </div>
->>>>>>> origin/20170822-lishihao-test
 </template>
 
 <script>
 
-<<<<<<< HEAD
-import HotCity from './hotcity.vue'
+import HotCity from './hotcity.vue';
+import cityHeader from './city_header.vue';
 
-=======
->>>>>>> origin/20170822-lishihao-test
     export default {
-        name: 'city-index',
         data () {
-            return {
-<<<<<<< HEAD
-              name: 'city-index'
-=======
-                cityClass: []
-            }
-        },
-        methods: {
-            handleChina() {
-                location.href='http://localhost:8080/#/city';
-            },
-            handleAbroad() {
-                location.href='http://localhost:8080/#/city/abroadCity';
->>>>>>> origin/20170822-lishihao-test
-            }
+            return {}
         },
         components: {
-          "hot-city": HotCity
+          "hot-city": HotCity,
+          "city-header": cityHeader
         }
     }
 
@@ -49,24 +25,5 @@ import HotCity from './hotcity.vue'
 
 
 <style scoped>
-<<<<<<< HEAD
-  
-=======
-  	.city-header {
-        line-height: 1.5rem;
-        background: skyblue;
-        text-align: center;
-    }
-    .city-area {
-        border: 1px solid white;
-        display: inline-block;
-        line-height: .5rem;
-        width: 2rem;
-        color: white;
-    }
-    .city-in-china {
-        background: white;
-        color: skyblue;
-    }
->>>>>>> origin/20170822-lishihao-test
+  	
 </style>

af9759b6bc668dc5715b102ec36e905aa07782f2
jiajingyun <1723491972@qq.com>
2017-08-26 11:52:42 +0800

    detal

--- src/pages/weekend/index.vue
+++ src/pages/weekend/index.vue
@@ -6,13 +6,18 @@
         </div>
         <div class="content">
             <div class="content-list" v-for='item in listInfo' :key='item.id'>
-                <img :src="item.img" class="content-list-img">
+                <div class="content-img">
+                    <img :src="item.img" class="content-list-img">
+                </div>
                 <p class="content-list-title">
                     <span class="name">{{item.title}}</span>
-                    <span class="price">
+                    <span class="price" v-if="item.show">
                         <span class="money">&yen;</span>
                         <span class="price-info">{{item.price}}</span>
                         <span class="min-price">起</span>
+                    </span>
+                    <span class="price" v-if="!item.show">
+                        产品详情
                     </span>
                 </p>
                 <p class="content-info">
@@ -34,56 +39,89 @@ export default {
             "title":"中国航空博物馆",
             "price":"112",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地",
+            'show':true
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
-            "price":"112",
+            "id":"2",
+            "img":"http://img1.qunarzz.com/sight/p0/1501/8d/8d99565a2dea08e7.water.jpg_640x276_188d04d3.jpg",
+            "title":"中国蜜蜂博物馆",
+            "price":"",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"小昆虫大视界",
+            "show":false
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
-            "price":"112",
+            "id":"3",
+            "img":"http://img1.qunarzz.com/sight/p0/1602/e6/e655d7ee86c7fd1390.water.jpg_640x276_8b20ff5e.jpg",
+            "title":"中国科学技术馆",
+            "price":"198",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"科学性、知识性、趣味性、互动性相结合的展",
+            "show":true
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
-            "price":"112",
+            "id":"4",
+            "img":"http://img1.qunarzz.com/sight/p25/201211/00/8a09e85dceb22cea93835fbb.jpg_640x276_b3ed91d8.jpg",
+            "title":"北京自然博物馆",
+            "price":"",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"打开通往自然的大门，去探索大自然的奥秘",
+            "show":false
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
+            "id":"5",
+            "img":"http://img1.qunarzz.com/sight/p81/201211/07/453d7d250c21a48693835fbb.jpg_640x276_23654982.jpg",
+            "title":"中国古动物馆",
             "price":"112",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"亚洲最大的古生物博物馆",
+            "show":true
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
-            "price":"112",
+            "id":"6",
+            "img":"http://img1.qunarzz.com/sight/p0/1705/3d/3d7d3740d157a9e9a3.img.jpg_640x276_2ed89e4c.jpg",
+            "title":"首都博物馆",
+            "price":"1",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
+            "content":"古老与现代并存，文明与科技兼顾",
+            "show":true
 
         },{
-            "id":"1",
-            "img":"http://img1.qunarzz.com/sight/p0/1501/28/2874bfd7fa4c5291.water.jpg_640x276_37669ce6.jpg",
-            "title":"中国航空博物馆",
+            "id":"7",
+            "img":"http://img1.qunarzz.com/wugc/p153/201204/20/18790ec397b2afe193835fbb.jpg_640x276_00c86e9b.jpg",
+            "title":"中国国家博物馆",
+            "price":"",
+            "link":'/detail',
+            "content":"历史与艺术并重，凝聚中国历史文化之精华",
+            "show":false
+
+        },{
+            "id":"8",
+            "img":"http://img1.qunarzz.com/sight/p0/1501/d0/d00d72664c3e73d8.water.jpg_640x276_f74664c2.jpg",
+            "title":"北京天文馆",
+            "price":"137",
+            "link":'/detail',
+            "content":"陪你去看流星雨落在这地球上",
+            "show":true
+
+        },{
+            "id":"9",
+            "img":"http://img1.qunarzz.com/sight/p0/1507/59/76e0dc86d4416f913d9d919ec79ffe0e.water.jpg_640x276_11e979a9.jpg",
+            "title":"北京汽车博物馆",
+            "price":"21",
+            "link":'/detail',
+            "content":"感受汽车的无穷魅力，享受汽车的无限乐趣",
+            "show":true
+
+        },{
+            "id":"10",
+            "img":"http://img1.qunarzz.com/sight/p0/1501/91/913a6c6e45ea135b.water.jpg_640x276_e91e3e8e.jpg",
+            "title":"警察博物馆",
             "price":"112",
             "link":'/detail',
-            "content":"亚洲最大以飞机文物为主的航空珍品荟萃地"
-
+            "content":"首都公安的巨大贡献和取得的辉煌成就",
+            "show":true
         }]
     }
   }
@@ -99,23 +137,32 @@ export default {
 	.museum-header{
         line-height:.88rem;
         position: relative;
-        font-size: .3rem;
         color:#fff;
         text-align: center;
         background:#00bcd4;
     }
     .header-left{
         position: absolute;
+        font-size: .36rem;
         color: #fff;
         left:.3rem;
+    }
+    .header-title{
+        font-size: .32rem
     }
     .content-list{
         margin-bottom: .2rem;
         background: #fff;
     }
+    .content-img{
+        width: 100%;
+        height:0;
+        padding-bottom: 43.3728%;
+    }
     .content-list-img{
+        display: block;
         width:100%;
-        height:3.12rem;
+        line-height: 100%;
     }
     .content-list-title{
         overflow: hidden;
@@ -133,19 +180,22 @@ export default {
     }
     .price{
         float: right;
-        font-size: .24rem;
+        font-size: .18rem;
     }
     .price-info{
         color: #ff8300;
-        font-size: .3rem;
+        font-size: .4rem;
         font-weight: bold;
         padding: 0 .08rem;
 
     }
+    .min-price{
+        font-size: .22rem;
+    }
     .content-info{
         padding: 0 .22rem;
         width:100%;
-        font-size: .24rem;
+        font-size: .26rem;
         padding-bottom: .36rem;
         color: #616161;
         overflow: hidden;

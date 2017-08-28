6027d65c907bf1ccf313b3d76b0d3045b19a35f3
admin <admin@qq.com>
2017-08-28 11:22:38 +0800

    111

--- src/pages/city/index.vue
+++ src/pages/city/index.vue
@@ -1,30 +1,18 @@
 <template>
-<<<<<<< HEAD
-
-    <div>
-        <div class="city-header">
-            <div class="city-header-area">
-                <span class="city-in-china city-area">国内</span>
-                <span class="city-area">海外</span>
-            </div>
-         </div>
-        <hot-city></hot-city>
-        <hot-city-abroad></hot-city-abroad>
-=======
     <div>
         <city-header></city-header>
-        
         <city-letter></city-letter> 
         <router-view></router-view>
->>>>>>> 1bfab2843daddb2654baf04bbc1f1e4f5b1912ce
     </div>
 </template> 
 
 <script>
-<<<<<<< HEAD
 
-    import HotCity from './hotcity.vue'
-    import HotCityAbroad from './hotcity-abroad.vue'
+    import HotCity from './hotcity.vue';
+    import HotCityAbroad from './hotcity-abroad.vue';
+    import cityHeader from './city_header.vue';
+    import CityLetter from './cityletter.vue';
+
     export default {
 
         data () {
@@ -32,29 +20,9 @@
         },
         components: {
             "hot-city" :HotCity,
-            "hot-city-abroad": HotCityAbroad
-        },
-        methods: {
-            handleChina() {
-                location.href='http://localhost:8080/#/city';
-            },
-            handleAbroad() {
-                location.href='http://localhost:8080/#/city/abroadCity';
-            }
-=======
-
-import cityHeader from './city_header.vue';
-import CityLetter from './cityletter.vue';
-
-
-    export default {
-        data () {
-            return {}
-        },
-        components: {
-          "city-header": cityHeader,
-          "city-letter": CityLetter
->>>>>>> 1bfab2843daddb2654baf04bbc1f1e4f5b1912ce
+            "hot-city-abroad": HotCityAbroad,
+            "city-letter": CityLetter,
+            "city-header": cityHeader
         }
     }
 
@@ -63,23 +31,4 @@ import CityLetter from './cityletter.vue';
 
 <style scoped>
 
-<<<<<<< HEAD
-  	.city-header {
-        line-height: 1.5rem;
-        background: skyblue;
-        text-align: center;
-    }
-    .city-area {
-        border: 1px solid white;
-        display: inline-block;
-        line-height: .5rem;
-        width: 2rem;
-        color: white;
-    }
-    .city-in-china {
-        background: white;
-        color: skyblue;
-    }
-=======
->>>>>>> 1bfab2843daddb2654baf04bbc1f1e4f5b1912ce
 </style>

cd570d37b713c106065b55e9a004f6c4c40c6c8d
lishanyangyu <861055025@qq.com>
2017-08-29 16:13:15 +0800

    sss

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import vueResource from 'vue-resource'
-import IScrollView from 'vue-iscroll-view'
-import IScroll from 'iscroll'
-import 'vue-event-calendar/dist/style.css' 
-import vueEventCalendar from 'vue-event-calendar'
-Vue.use(IScrollView, IScroll)
-Vue.use(vueResource);
-Vue.use(vueEventCalendar, {locale: 'en',color: 'pink'}) 
-fastclick.attach(document.body);
+
 Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,

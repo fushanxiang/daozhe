724b10233b7945293d8569f057634c47fbf51bdc
Mr_patience <823130507@qq.com>
2017-08-31 20:50:46 +0800

    a

--- src/router/index.js
+++ src/router/index.js
@@ -2,75 +2,52 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/pages/index/index'
 import City from '@/pages/city/index'
-<<<<<<< HEAD
 import TicketSale from '@/pages/ticketSale/index'
 import TicketDetail from '@/pages/ticketDetail/index'
 import Weekend from '@/pages/weekend/index'
 import SecKill from '@/pages/seckill/index'
-
-=======
 import Productdetail from '@/pages/productdetail/index'
 import search from '@/search/search'
 import List from '@/pages/list/index'
-import Weekend from '@/pages/weekend/index'
-import SecKill from '@/pages/seckill/index'
-
-
-Vue.use(Router)
->>>>>>> origin/master
 
 Vue.use(Router)
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index
-    },{
-      path: '/weekend',
-      name: 'weekend',
-      component: Weekend
-    },{
-      path:'/seckill',
-      name:'seckill',
-      component:SecKill
-    },{
-      path: '/city',
-      name: 'city',
-<<<<<<< HEAD
-      component: City
-    },
-    {
-      path: '/ticketSale',
-      name: 'ticketSale',
-      component: TicketSale
-
-    },
-    {
-      path: '/ticketDetail/:id',
-      name: 'ticketDetail',
-      component: TicketDetail
-    }]
-=======
-      component: City,
-    },
-    {
-      path: '/productdetail',
-      name: 'productdetail',
-      component: Productdetail
-    },
-    {
-      path: '/list',
-      name: 'list',
-      component: List
-
-    },{
-      path: '/search',
-      name: 'search',
-      component: search
-
-    }]
-
-
->>>>>>> origin/master
+	routes: [
+	{
+		path: '/',
+		name: 'index',
+		component: Index
+	},{
+		path: '/weekend',
+		name: 'weekend',
+		component: Weekend
+	},{
+		path:'/seckill',
+		name:'seckill',
+		component:SecKill
+	},{
+		path: '/city',
+		name: 'city',
+		component: City
+	},{
+		path: '/ticketSale',
+		name: 'ticketSale',
+		component: TicketSale
+	},{
+		path: '/ticketDetail/:id',
+		name: 'ticketDetail',
+		component: TicketDetail
+	},{
+		path: '/productdetail',
+		name: 'productdetail',
+		component: Productdetail
+	},{
+		path: '/list',
+		name: 'list',
+		component: List
+	},{
+		path: '/search',
+		name: 'search',
+		component: search
+	}]
 })
